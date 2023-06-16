de819083895fb9d81d8959a700e0f1e5f1bd7fe9
cristofer <cristofer.albornoz@virginiogomez.cl>
2023-06-15 22:32:15 -0400

    categoria,juego y usuario

---
+++ JuegosSteam/Controllers/JuegoController.cs
@@ -0,0 +1,123 @@
+﻿using JuegosSteam.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuegosSteam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JuegoController : ControllerBase
+    {
+
+        private readonly BaseSteamContext db = new();
+
+        [HttpGet]
+        public async Task<IActionResult> GetJuego()
+        {
+            Response response = new();
+            try
+            {
+                if (db.Juegos == null)
+                {
+                    response.Message = "La tabla no esta activa";
+                    return NotFound(response);
+                }
+                var juegos = await db.Juegos.Select(
+                    x => new
+                    {
+                        x.Id,
+                        x.Nombre,
+                        x.Precio,
+                        x.Categoria,
+                        x.Plataforma,
+                        x.Desarrollador,
+                        x.UsuarioRegistrado,
+                        x.Editor
+
+                    }).ToListAsync();
+                if (juegos != null)
+                {
+                    if (juegos.Count == 0)
+                    {
+                        response.Message = "No hay registros";
+                    }
+                    response.Success = true;
+                    response.Data = juegos;
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+
+                response.Message = ex.ToString();
+                return BadRequest(response);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetJuego(int id)
+        {
+            Response response = new();
+            try
+            {
+                //find busca solo por el identificador
+                var buscarJuego = await db.Juegos.FindAsync(id);
+                if (buscarJuego == null)
+                {
+                    response.Message = "No existe registron con ese id";
+                    return NotFound(response);
+                }
+                else
+                {
+                    response.Success = true;
+                    response.Data = buscarJuego;
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Message = "Error: " + ex.ToString();
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Juego>> PostJuego(Juego juego)
+        {
+
+            db.Juegos.Add(juego);
+            await db.SaveChangesAsync();
+            Response response = new();
+            response.Success = true;
+            response.Message = "Guardado con éxito";
+
+            //return Ok(response); //retorna el mensaje que entregamos
+            //retorna al getid de sucursal
+            return CreatedAtAction("GetJuego", new { id = juego.Id }, juego);
+        }
+        // DELETE: api/Sucursal/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJuego(int id)
+        {
+            Response response = new();
+            var buscarJuego = await db.Juegos.FindAsync(id);
+            if (buscarJuego != null)
+            {
+
+                db.Remove(buscarJuego);
+                await db.SaveChangesAsync();
+                response.Message = "El registro se ha eliminado con éxito";
+                response.Success = true;
+                return Ok(response);
+            }
+            response.Message = "No se encuntra el id";
+            return NotFound(response);
+        }
+
+
+    }
+}
+
+
+
---
+++ JuegosSteam/Controllers/UsuarioController.cs
@@ -0,0 +1,119 @@
+﻿using JuegosSteam.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuegosSteam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        private readonly BaseSteamContext db = new();
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsuario()
+        {
+            Response response = new();
+            try
+            {
+                if (db.Usuarios == null)
+                {
+                    response.Message = "La tabla no esta activa";
+                    return NotFound(response);
+                }
+                var usuarios = await db.Usuarios.Select(
+                    x => new
+                    {
+                        x.Id,
+                        x.Nombre,
+                        x.Rut,
+                        x.Telefono,
+                        x.Correo,
+                        x.RolesNavigation
+                    }).ToListAsync();
+                if (usuarios != null)
+                {
+                    if (usuarios.Count == 0)
+                    {
+                        response.Message = "No hay registros";
+                    }
+                    response.Success = true;
+                    response.Data = usuarios;
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+
+                response.Message = ex.ToString();
+                return BadRequest(response);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUsuario(int id)
+        {
+            Response response = new();
+            try
+            {
+                //find busca solo por el identificador
+                var buscarUsuario = await db.Usuarios.FindAsync(id);
+                if (buscarUsuario == null)
+                {
+                    response.Message = "No existe registron con ese id";
+                    return NotFound(response);
+                }
+                else
+                {
+                    response.Success = true;
+                    response.Data = buscarUsuario;
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Message = "Error: " + ex.ToString();
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+
+            db.Usuarios.Add(usuario);
+            await db.SaveChangesAsync();
+            Response response = new();
+            response.Success = true;
+            response.Message = "Guardado con éxito";
+
+            //return Ok(response); //retorna el mensaje que entregamos
+            //retorna al getid de sucursal
+            return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuario);
+        }
+        // DELETE: api/Sucursal/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+            Response response = new();
+            var buscarUsuario = await db.Usuarios.FindAsync(id);
+            if (buscarUsuario != null)
+            {
+
+                db.Remove(buscarUsuario);
+                await db.SaveChangesAsync();
+                response.Message = "El registro se ha eliminado con éxito";
+                response.Success = true;
+                return Ok(response);
+            }
+            response.Message = "No se encuntra el id";
+            return NotFound(response);
+        }
+
+
+    }
+}
+
+
